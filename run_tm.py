e082abf3126321e3999b1ce7844c6b9b4eb9bcfb
Tobias Schlöder <tobias.schloeder@kit.edu>
2021-12-21 16:37:50 +0100

    Intensities and osc. strengths added to results.yml

--- run_tm.py
+++ run_tm.py
@@ -1,4 +1,4 @@
-import os,sys,yaml
+import os, sys, yaml, shutil
 
 import turbomole_functions as tm
 import ase.io
@@ -22,6 +22,7 @@ def get_settings_from_rendered_wano(filename='rendered_wano.yml'):
     
     settings['title']=wano_file['Title']
     settings['follow-up']=wano_file['Follow-up calculation']
+    settings['use old struct']=wano_file['Molecular structure']['Use old structure']
     settings['structure file type']=wano_file['Molecular structure']['Structure file type']
     settings['int coord']=wano_file['Molecular structure']['Internal coordinates']
     settings['basis set']=wano_file['Basis set']['Basis set type']
@@ -30,11 +31,11 @@ def get_settings_from_rendered_wano(filename='rendered_wano.yml'):
     settings['charge']=wano_file['Initial guess']['G1']['Charge']
     settings['multiplicity']=wano_file['Initial guess']['G1']['Multiplicity']
     settings['scf iter']=60
-    settings['max scf iter']=wano_file['DFT options']['Max SCF iterations']
-    settings['use ri']=wano_file['DFT options']['Use RI']
-    settings['ricore']=wano_file['DFT options']['Memory for RI']
+    settings['max scf iter']=wano_file['DFT options']['Adv options']['Max SCF iterations']
+    settings['use ri']=wano_file['DFT options']['Adv options']['Use RI']
+    settings['ricore']=wano_file['DFT options']['Adv options']['Memory for RI']
     settings['functional']=wano_file['DFT options']['Functional']
-    settings['grid size']=wano_file['DFT options']['Integration grid']
+    settings['grid size']=wano_file['DFT options']['Adv options']['Integration grid']
     settings['disp']=disp_dict[wano_file['DFT options']['vdW correction']]
     settings['cosmo']=wano_file['DFT options']['COSMO calculation']
     settings['epsilon']=wano_file['DFT options']['Rel. permittivity']
@@ -80,28 +81,39 @@ if __name__ == '__main__':
     settings=get_settings_from_rendered_wano()
 
     if settings['follow-up']: 
-        os.system('mkdir old_results; tar -xf old_calc.tar.xz -C old_results')
-        os.system('cp old_results/coord %s'%(coord_file))
+        os.mkdir('old_results')
+        os.system('tar -xf old_calc.tar.xz -C old_results')
+
+        if settings['use old struct']: 
+            shutil.copy('old_results/coord',coord_file)
+
         old_settings = get_settings_from_rendered_wano(filename='old_results/rendered_wano.yml')
         settings['title']=old_settings['title']
         if settings['use old mos']: settings['multiplicity']=old_settings['multiplicity']
+    
+    if not os.path.isfile(coord_file):
+        if settings['structure file type'] == 'Turbomole coord':
+            os.rename('initial_structure',coord_file)
 
-    else:
-        if settings['structure file type'] == 'Turbomole coord': os.rename('initial_structure',coord_file)
         else:
             if settings['structure file type'] == 'Gaussian input':
                 ginp = GaussianInput.from_file('initial_structure')
                 xyz.XYZ(ginp.molecule).write_file('initial_structure')
+
             os.system('x2t initial_structure > %s'%(coord_file))
 
     if not settings['use old mos']:
-        if settings['charge from file'] and settings['structure file type'] == 'Gaussian input': settings['charge'], settings['multiplicity'] = ginp.charge, ginp.spin_multiplicity
+        if settings['charge from file'] and settings['structure file type'] == 'Gaussian input':
+            settings['charge'], settings['multiplicity'] = ginp.charge, ginp.spin_multiplicity
+
         else:
-            n_el=sum(ase.io.read(coord_file).numbers)-settings['charge']
-            settings['multiplicity']=sanitize_multiplicity(settings['multiplicity'],n_el)
+            n_el = sum(ase.io.read(coord_file).numbers)-settings['charge']
+            settings['multiplicity'] = sanitize_multiplicity(settings['multiplicity'],n_el)
 
     tm.inputprep('define',tm.make_define_str(settings,coord_file))
-    if settings['follow-up']: os.system('rm -rf old_results')
+
+    if settings['follow-up']:
+        os.system('rm -rf old_results')
 
     if settings['cosmo']:
         if settings['follow-up']:
@@ -136,29 +148,49 @@ if __name__ == '__main__':
     with open('eiger.out') as infile: results_dict['homo-lumo gap'] = float(infile.readlines()[14].split()[2])
 
     if settings['tddft']:
-        results_dict['exc_type']=settings['exc state type']
-        exc_energies=[]
+        results_dict['exc_type'] = settings['exc state type']
+        excitations = []
+
         with open('exspectrum') as infile:
-            exc_lines=infile.readlines()[-settings['num exc states']:]
-        for exc_line in exc_lines: exc_energies.append(float(exc_line.split()[2]))
-        results_dict['exc_energies']=exc_energies
+            exc_lines = infile.readlines()[-settings['num exc states']:]
+
+        for exc_line in exc_lines:
+            exc_dict = {'exc energy': float(exc_line.split()[2]), 'osc. strength': float(exc_line.split()[-2])}
+            excitations.append(exc_dict)
+        results_dict['electronic excitations'] = excitations
+
     elif settings['freq']:
-        vib_freq=[]
+        vib_spectrum = []
+
         with open('aoforce.out') as infile:
-            ao_lines=infile.readlines()
-        for ao_line in ao_lines:
-            if 'frequency  ' in ao_line:
-                for freq in ao_line.split()[1:]:
-                    if 'i' in freq: vib_freq.append(-float(freq.replace('i','')))
-                    else: vib_freq.append(float(freq))
-            if 'zero point' in ao_line: results_dict['ZPE']=float(ao_line.split()[6])
-        results_dict['vibrational frequencies']=vib_freq
+            ao_lines = infile.readlines()
 
-    with open('turbomole_results.yml','w') as outfile: yaml.dump(results_dict,outfile)
+        for i in range(len(ao_lines)):
+            if 'frequency  ' in ao_lines[i]:
+                frequencies = ao_lines[i].split()[1:]
+                intensities = ao_lines[i+6].split()[2:]
+                for j in range(len(frequencies)):
+                    if 'i' in frequencies[j]:
+                        freq = -float(frequencies[j].replace('i',''))
 
-    output_files=['alpha','auxbasis','basis','beta','control','coord','energy','forceapprox','gradient','hessapprox','mos','optinfo','rendered_wano.yml','sing_a','trip_a','unrs_a'] # implement symmetry: irrep names in [sing,trip,unrs]_irrep
+                    else:
+                        freq = float(frequencies[j])
+
+                    freq_dict = {'frequency': freq, 'intensity': float(intensities[j])}
+                    vib_spectrum.append(freq_dict)
+
+            if 'zero point' in ao_lines[i]:
+                results_dict['ZPE'] = float(ao_lines[i].split()[6])
+
+        results_dict['vibrational frequencies'] = vib_spectrum
+
+    with open('turbomole_results.yml','w') as outfile:
+        yaml.dump(results_dict,outfile)
+
+    output_files = ['alpha','auxbasis','basis','beta','control','coord','energy','forceapprox','gradient','hessapprox','mos','optinfo','rendered_wano.yml','sing_a','trip_a','unrs_a'] # implement symmetry: irrep names in [sing,trip,unrs]_irrep
     for filename in output_files:
-        if not os.path.isfile(filename): output_files.remove(filename)
+        if not os.path.isfile(filename):
+            output_files.remove(filename)
 
     os.system('tar -cf results.tar.xz %s'%(' '.join(output_files)))
     os.system('t2x coord > final_structure.xyz')
--- turbomole_functions.py
+++ turbomole_functions.py
@@ -4,12 +4,18 @@ import subprocess
 #function definitions
 
 def utf8_enc(var):
-    if sys.version_info >= (3, 0): return var.encode('utf-8')
-    else: return var
+    if sys.version_info >= (3, 0):
+        return var.encode('utf-8')
+
+    else:
+        return var
 
 def utf8_dec(var):
-    if sys.version_info >= (3, 0): return var.decode('utf-8')
-    else: return var
+    if sys.version_info >= (3, 0):
+        return var.decode('utf-8')
+
+    else:
+        return var
 
 def make_define_str(settings,coord):
     
@@ -19,54 +25,79 @@ def make_define_str(settings,coord):
         same_basis = old_settings['Basis set']['Basis set type'] == settings['basis set']
 
     if not (settings['use old mos'] and same_basis):
-        define_string='\n%s\na %s\n'%(settings['title'],coord)
+        define_string = '\n%s\na %s\n'%(settings['title'],coord)
         #implement symmetry
-        if settings['int coord']: define_string+='ired\n*\n'
-        else: define_string+='*\nno\n'
+        if settings['int coord']:
+            define_string+='ired\n*\n'
 
-        define_string+='b all %s\n*\n'%(settings['basis set']) # add options for basis sets (different for different atoms)
+        else:
+            define_string+='*\nno\n'
+
+        define_string += 'b all %s\n*\n'%(settings['basis set']) # add options for basis sets (different for different atoms)
 
         if not settings['use old mos']:
-            define_string+='eht\n\n'
-            define_string+='%i\n'%(settings['charge'])
+            define_string += 'eht\n\n'
+            define_string += '%i\n'%(settings['charge'])
             #implement symmetry
-            if settings['multiplicity'] < 3: define_string+='\n\n\n'
-            else: define_string+='n\nu %i\n*\n\n'%(settings['multiplicity']-1)
-        else: 
-            define_string+='use old_results/control\n\n'
 
-        define_string+='scf\niter\n%i\n\n'%(settings['scf iter'])
-        if settings['use ri']: define_string+='ri\non\nm %i\n\n'%(settings['ricore'])
-        if settings['functional'] != 'None': define_string+='dft\non\nfunc %s\ngrid %s\n\n'%(settings['functional'],settings['grid size'])
-        if settings['disp'] != 'off': define_string+='dsp\n%s\n\n'%(settings['disp'])
+            if settings['multiplicity'] < 3:
+                define_string += '\n\n\n'
+
+            else:
+                define_string += 'n\nu %i\n*\n\n'%(settings['multiplicity']-1)
+        else: 
+            define_string += 'use old_results/control\n\n'
+
+        define_string += 'scf\niter\n%i\n\n'%(settings['scf iter'])
+        
+        if settings['use ri']:
+            define_string += 'ri\non\nm %i\n\n'%(settings['ricore'])
+
+        if settings['functional'] != 'None':
+            define_string += 'dft\non\nfunc %s\ngrid %s\n\n'%(settings['functional'],settings['grid size'])
+
+        if settings['disp'] != 'off':
+            define_string += 'dsp\n%s\n\n'%(settings['disp'])
 
         if settings['tddft']:
-            define_string+='ex\n'
-            if settings['multiplicity'] > 1: define_string+='urpa\n*\n'
-            else: define_string+='rpa%s\n*\n'%(settings['exc state type'][0].lower())
-            define_string+='a %i\n*\n'%(settings['num exc states'])
-            define_string+='*\n\n'
+            define_string += 'ex\n'
+
+            if settings['multiplicity'] > 1:
+                define_string += 'urpa\n*\n'
+
+            else:
+                define_string += 'rpa%s\n*\n'%(settings['exc state type'][0].lower())
+
+            define_string += 'a %i\n*\n'%(settings['num exc states'])
+            define_string += '*\n\n'
             #implement symmetry
-        define_string+='*\n'
+
+        define_string += '*\n'
     
     else:
-        output_files=['alpha','auxbasis','basis','beta','control','hessapprox','mos']
+        output_files = ['alpha','auxbasis','basis','beta','control','hessapprox','mos']
         for filename in output_files:
             if os.path.isfile('old_results/%s'%(filename)): os.system('cp old_results/%s .'%(filename))
-        os.system('cp coord_0 coord')
-        define_string='\n\n\n\n\n'
-        if settings['use ri']: define_string+='ri\non\nm %i\n\n'%(settings['ricore'])
-        else: define_string+='ri\noff\n\n'
+
+        shutil.copy('coord_0','coord')
+        define_string = '\n\n\n\n\n'
+
+        if settings['use ri']:
+            define_string += 'ri\non\nm %i\n\n'%(settings['ricore'])
+
+        else:
+            define_string += 'ri\noff\n\n'
+
         if settings['functional'] != 'None': define_string+='dft\non\nfunc %s\ngrid %s\n\n'%(settings['functional'],settings['grid size'])
         else: define_string+='dft\noff\n\n'
         define_string+='dsp\n%s\n\n'%(settings['disp'])
         
         if not settings['tddft']:
-            if old_settings['Type of calculation']['Excited states calculation']:
+            if old_settings['Type of calculation']['Excited state calculation']:
                 os.system('sed -i \'s/#$max/$max/g\' control')
                 for dg in 'soes','scfinstab','rpacor','denconv': os.system('kdg %s'%(dg))
 
-        elif not old_settings['Type of calculation']['Excited states calculation']:
+        elif not old_settings['Type of calculation']['Excited state calculation']:
             define_string+='ex\n'
             if settings['multiplicity'] > 1: define_string+='urpa\n*\n'
             else: define_string+='rpa%s\n*\n'%(settings['exc state type'][0].lower())
@@ -75,9 +106,14 @@ def make_define_str(settings,coord):
             #implement symmetry
         else: 
             define_string+='ex\n'
+
             if old_settings['Type of calculation']['TDDFT options']['Type of excited states'] != settings['exc state type']:
-                if settings['multiplicity'] > 1: define_string+='urpa\n'
-                else: define_string+='rpa%s\n'%(settings['exc state type'][0].lower())
+                if settings['multiplicity'] > 1:
+                    define_string+='urpa\n'
+
+                else:
+                    define_string+='rpa%s\n'%(settings['exc state type'][0].lower())
+
             define_string+='*\n'
             define_string+='a %i\n*\n'%(settings['num exc states'])
             define_string+='*\n\n'
